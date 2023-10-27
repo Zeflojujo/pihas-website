b8b118cf36e2dc974dc1911a0ae386a7a896b73e
josephat <zeflojujo52@gmail.com>
2023-10-27 07:50:45 -0700

    add tailwind in the project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 // import "./styles.css";
 
 import React from "react";
+import "index.css";
 import {BrowserRouter,HashRouter, Routes, Route} from "react-router-dom";
 import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import About from "./components/pages/About";
 import News from "./components/pages/News";
 import Profile from "./components/pages/Profile";
+import SlideShow from "./components/pages/SlideShow";
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           <Route path="/about" element={<About/>}/>
           <Route path="/news" element={<News/>}/>
           <Route path="/profile" element={<Profile/>}/>
+          <Route path="/slide" element={<SlideShow/>}/>
         </Routes>
       </HashRouter>
       
